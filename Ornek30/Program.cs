800d7c5abd48d93da5b1deb580f0657ef812d090
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2020-12-07 11:44:07 +0300

    Sayıyı Tekrarlatarak Yazdırma

---
+++ Ornek30/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Sayıyı tekrarlatarak yazdırma";
+            Console.WriteLine("Bir sayı giriniz: ");
+            long sayi = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine();
+            for (int i = 1; i <=(2*sayi); i++)
+            {
+                Console.WriteLine($"{i}.kere yazıldı : {sayi}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
