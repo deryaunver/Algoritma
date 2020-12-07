9a878b32b48279406c5c8b7fda5f82b4c6a682ae
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2020-12-07 11:38:22 +0300

    Tam Bölünüyor mu?

---
+++ Ornek29/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "tAM BÖLÜNÜYOR MU?";
+            Console.WriteLine("BİRİNCİ SAYIYI GİRİNİZ?");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("iKİNCİ SAYIYI GİRİNİZ?");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+            double sonuc=sayi1/sayi2;
+            if (sonuc==0)
+            {
+                Console.WriteLine(" birinci sayı ikinci sayıya Tam bölünüyor!".ToUpper());
+            }
+            else
+            {
+                Console.WriteLine("birinci sayı ikinci sayıya Tam bölünmüyor".ToUpper());
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
