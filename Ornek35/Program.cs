1d9c871e6f1635339dbd6d2c6c2104a011799a07
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2020-12-09 11:39:51 +0300

    Fibonacci Dizisi

---
+++ Ornek35/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Fibonacci Dizisi".ToUpper();
+            // 0 1 1 2 3 5 8 11 ...
+            int index0 = 0;
+            int index1 = 1;
+           
+            Console.Write("Fibonacci dizisinin ilk kaç elemanı hesaplatılsın: ");
+            int hesaplat = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(index0);
+            Console.WriteLine(index1);
+            int yeniSayi;
+            for (int i = 3; i <=10; i++)
+            {
+                yeniSayi = index0 + index1;
+                Console.WriteLine(yeniSayi);
+                index0 = index1;
+                index1 = yeniSayi;
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
