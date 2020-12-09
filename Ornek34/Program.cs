2befc882e12618ff36cc3e6ca675c6a9efbd82bf
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2020-12-09 11:24:20 +0300

    Faktöriyel Hesaplama

--- Ornek34/Program.cs
+++ Ornek34/Program.cs
@@ -10,6 +10,16 @@ namespace Ornek34
     {
         static void Main(string[] args)
         {
+            Console.Title = "Faktöriyel Hesaplama".ToUpper();
+            Console.Write("Faktöriyeli hesaplatmak istediğiniz sayıyı giriniz: ");
+            long sayi = Convert.ToInt64(Console.ReadLine());
+            long faktHesapla = 1;
+            for (int i = 1; i <=sayi; i++)
+            {
+                faktHesapla = faktHesapla * i;
+            }
+            Console.WriteLine($"{sayi} Sayısının Faktöriyeli: {faktHesapla}");
+            Console.ReadKey();
         }
     }
 }
