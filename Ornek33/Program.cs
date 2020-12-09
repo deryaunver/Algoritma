631d0ac9a62e4c46d33044e7f14f1b106477328a
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2020-12-09 10:55:10 +0300

    Girilen Sayıların Küplerini Hesaplama

---
+++ Ornek33/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Girilen Sayıların Küplerini Hesaplama".ToUpper();
+            Console.WriteLine("Başlangıç sayısını giriniz: ");
+            long baslangic = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Bitiş sayısını giriniz: ");
+            long bitis = Convert.ToInt64(Console.ReadLine());
+            long KupHesapla ;
+            for (long i = baslangic; i < bitis; i++)
+            {
+                KupHesapla = i * i * i;
+                Console.WriteLine($"{i}->Küpü: {KupHesapla}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
