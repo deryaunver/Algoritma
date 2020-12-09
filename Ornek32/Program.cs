4225fb4bb16e7e40b204893d8bc65924fefe44c3
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2020-12-09 10:49:44 +0300

    Dörderli Geriye Saydırma

---
+++ Ornek32/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "DÖRDERLİ GERİYE SAYDIRMA";
+            Console.WriteLine("Başlangıç sayısını giriniz: ");
+            long baslangic = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Başlangıç sayısını giriniz: ");
+            long bitis = Convert.ToInt64(Console.ReadLine());
+            Console.Write($"{bitis} - {baslangic} geriye saydırma: ");
+            Console.WriteLine();
+            for (long i =bitis; i >=baslangic; i-=4)
+            {
+                Console.Write(i+" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
