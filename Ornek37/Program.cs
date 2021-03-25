dd20a27eb644f53bfad8a2176babbc3d0c9e2a76
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2021-03-25 12:33:34 +0300

    Klavyeden 10 adet sayı alıp ekrana yazdırma (dizi kullanarak)

---
+++ Ornek37/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "♥ Kullanıcıdan aldığı sayıyı dizi ile ekrana yazdırma ♥";
+            int[] yazdır = new int[11];
+            for (int i = 1; i <=10; i++)
+            {
+                Console.WriteLine($"lütfen {i}. sayıyı giriniz:");
+                int sayiAl = int.Parse(Console.ReadLine());
+                yazdır[i] = sayiAl;
+            }
+
+            for (int i = 1; i < yazdır.Length; i++)
+            {
+                Console.Write(yazdır[i]+" ");
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
