07114458514230901d42190df21588cc7a7f6595
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2021-03-31 11:55:52 +0300

    klavyeden girilen metinin 3 karakterden başlayarak 5 karakterinin yazdırma

---
+++ Ornek39/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Meitini belli kısımlarını yazdırma";
+
+            Console.WriteLine("Bir metin değeri giriniz");
+            string metin = Console.ReadLine();
+
+            #region Yöntem1
+            //for (int i = 0; i < metin.Length; i++)
+            //{
+
+            //    if (i == 6)
+            //    {
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        Console.Write(metin[i + 2]);
+            //    }
+            //}
+            #endregion
+
+            #region Yöntem2
+            Console.WriteLine(metin.Substring(3, 5)); 
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
