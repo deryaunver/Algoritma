179e9845ed4a51f0d85924b21bb2c614bcbab6ed
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2021-03-31 12:02:36 +0300

    klavyeden girilen metinin 3. karakterden başlayarak 5 karakteri ekrana yazdıran ama girilen metin 8 karakterden az ise uyarı veren kod parçası

---
+++ Ornek40/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool durum =true;
+            while (durum)
+            {
+                Console.WriteLine("bir metin girin :");
+                string metin = Console.ReadLine();
+                if (metin.Length < 8)
+                {
+                    Console.WriteLine(" girilen metnin uzunluğu en az 8 karakter olmalıdır.");
+
+                }
+                else
+                {
+                    Console.WriteLine(metin.Substring(3, 5));
+                    durum = false;
+                }
+
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
