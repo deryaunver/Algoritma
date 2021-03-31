1e3a2a80d28dd8d5194e8fc249a8b710b723ebe6
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2021-03-31 11:46:39 +0300

    Klavyeden girilen metinin karakterlerini büyük harfe çevirme

---
+++ Ornek38/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Metini Büyük Harfe Çevirme";
+
+            Console.WriteLine("Bir metin giriniz:");
+            string metin = Console.ReadLine();
+       
+            Console.WriteLine(metin.ToUpper());
+
+            Console.ReadKey();
+
+        }
+    }
+}
