2509bcddc900e59c397ef9713712b93bd0c3ff12
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2021-03-31 12:17:05 +0300

    rasgele sayi üetimi

---
+++ Ornek41/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Rasgele Sayı Üretmek";
+
+            Random random = new Random();
+            
+            Console.WriteLine("zar 1: "+random.Next(1, 6)+" "+ "zar 2: " + random.Next(1, 6));
+       
+            Console.ReadKey();
+
+        }
+    }
+}
