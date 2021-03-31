b490319e785d7e33e16cc245565f78193df2608e
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2021-03-31 13:01:56 +0300

    Kullanıcıdan sayı alarak,aldığı sayılardan tek olanları toplama ekleyip çift olanları toplamdan çıkaran kod parçası ( toplam 100 e ulaşınca program sonlansın )

---
+++ Ornek44/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0;
+            bool durum = true;
+            while (durum)
+            {
+                Console.WriteLine("Bir Sayı Girin :");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                if (sayi % 2 != 0)
+                {
+                    toplam = toplam + sayi;
+                }
+                else if (sayi == 0)
+                {
+                    durum = false;
+                }
+                else
+                {
+                    toplam = toplam - sayi;
+                }
+
+                if (toplam >= 100)
+                {
+                    durum = false;
+                }
+
+
+            }
+            Console.WriteLine("Toplam = " + toplam);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
