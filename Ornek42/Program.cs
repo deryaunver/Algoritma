e2c2e13dae3117fc1847fbca3d6be7dbe20f6bb3
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2021-03-31 12:44:11 +0300

    10 kişilik bir kisi listesi içerisinden çekiliş yaparak 3 kişiyi ekrana yazdırma

---
+++ Ornek42/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ornek42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Çekiliş Programı";
+            ArrayList liste = new ArrayList();
+            string[] katilimcilar =
+                {"Derya", "Emrah", "Evren", "Duygu", "Nurgül", "Şakir", "Fatih", "Ayşenur", "Nursel", "Yavus"};
+
+
+            liste.AddRange(katilimcilar);
+            Random random = new Random();
+            int endex = 0;
+            int x = 0;
+
+            string[] kazananlar = new string[4];
+            for (x = 0; x <= 2; x++)
+            {
+                endex = random.Next(0, katilimcilar.Length - 1);
+                kazananlar[x] = katilimcilar[endex].ToString();
+                liste.RemoveAt(endex);
+            }
+
+            for (x = 0; x <= 2; x++)
+            {
+                Console.WriteLine(x + 1 + ".talihli:" + kazananlar[x]);
+            }
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
