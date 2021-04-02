f6c8b572a4ec8fc0d653ac2e30fccccf461c40da
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2021-04-02 18:27:40 +0300

    Fibonacci Hesabı

--- Ornek47/Program.cs
+++ Ornek47/Program.cs
@@ -8,7 +8,7 @@ namespace Ornek47
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)  
         {
             Console.Title = "Fibonacci Hesabı";
 
