d661e5758efa3042ef916bb244a08e7f05906b2f
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2021-04-02 18:26:57 +0300

    Asal kontrolu 2

---
+++ Ornek47/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Fibonacci Hesabı";
+
+            Console.WriteLine("bir sayı giriniz:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            // Fibonacci: 0 1 1 2 3
+            int sabit1 = 0;
+            int sabit2 = 1;
+            Console.Write($"{sabit1} {sabit2} " );
+            Console.Write(sabit1+sabit2);
+            int yenideger=0;
+            int sonuc = 0;
+  
+            for (int i = 0; i < sayi; i++)
+            {
+                yenideger = sabit1 + sabit2;
+                sabit1 = sabit2;
+                sabit2 = yenideger;
+                sonuc = yenideger + sabit1;
+                Console.Write(" "+sonuc);
+            }
+            Console.ReadLine();
+    
+        }
+    }
+}
---
+++ Ornek48/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Asal Sayi Kontrolü";
+
+            Console.WriteLine("Bir sayı giriniz:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            bool durum = true;
+            for (int i = 2; i <=sayi-1; i++)
+            {
+                if (sayi%i==0)
+                {
+                    durum = false;
+                }
+            }
+
+            if (durum==true)
+            {
+                Console.WriteLine("sayı asaldır");
+            }
+            else
+            {
+                Console.WriteLine("sayı asal değildir");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
