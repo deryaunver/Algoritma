732489fa9759b69d121cd60da479f5830d4a376b
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2021-04-02 19:49:27 +0300

    Klavyeden girilen sayının hangi sayının faktöriyeli olduğunu bulan kod

---
+++ Ornek49/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = " girilen sayının hangi sayının faktöriyeline eşit olduğunu bulma";
+            Console.WriteLine("Sayı gir");
+            int sayı = Convert.ToInt32(Console.ReadLine());
+            bool durum = true;
+            int sayac = 0;
+            while (durum)
+            {
+                sayac = sayac + 1;
+                sayı = sayı / sayac;
+                if (sayı == 1)
+                {
+                    Console.WriteLine(sayac);
+                    break;
+                }
+              
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
