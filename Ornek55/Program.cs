29029435c14475573d4adacec6cd1017a0606549
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2021-04-03 11:12:54 +0300

    Palindrom-Vektörel

---
+++ Ornek55/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Palindrom-Vektörel";
+            Console.WriteLine("Bir cümle giriniz:");
+            string cumle = Console.ReadLine();
+
+            string tersCumle = "";
+
+            for (int i = cumle.Length - 1; i >= 0; i++)
+            {
+                tersCumle += cumle[i];
+            }
+
+            if (cumle == tersCumle)
+            {
+                Console.WriteLine("Palindromdur");
+            }
+            else
+            {
+                Console.WriteLine("Palindrom değildir");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
