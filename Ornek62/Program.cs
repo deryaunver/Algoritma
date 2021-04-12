2c62ec76b2d860efaed14df40c522f54b1b0f53f
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2021-04-12 12:27:40 +0300

    Liste kullanmadan n tane  girilen elemanı tersten yazdırma

---
+++ Ornek62/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek62
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Liste kullanmadan  girilen elemanları tersten yazdırma";
+            string[] kelimeler= new string[0];
+            bool durum = true;
+            while (durum)
+            {
+                Console.WriteLine("Kelime girin:");
+                string kelime = Console.ReadLine();
+                Array.Resize(ref kelimeler, kelimeler.Length + 1);// diziyi her seferinde boyutlandırdık
+                kelimeler[kelimeler.Length - 1] = kelime;
+                if (kelime==" ")
+                {
+                    for (int i = kelimeler.Length-1; i>=0; i--)
+                    {
+                        Console.WriteLine(kelimeler[i]);
+
+                    } 
+
+                    durum = false;
+                }
+
+            }
+          
+            Console.ReadKey();
+
+        }
+    }
+}
